a710392158a724eed7a5a25e857e8dd15116a2f2
Diogo Sousa <diogo.sousa23@outlook.com>
2019-12-15 18:42:17 +0000

    msg

--- my-new-project/src/app/components/create-new-user/create-new-user.component.ts
+++ my-new-project/src/app/components/create-new-user/create-new-user.component.ts
@@ -12,7 +12,9 @@ import { User } from 'src/app/models/user';
 @Injectable()
 export class CreateNewUserComponent implements OnInit {
 userName: String;
+booleanUsernameAvailable: boolean = true;
 email: String;
+booleanEmailAvailable: boolean = true;
 age: number;
 password: String;
 rePassword: String;
@@ -31,20 +33,21 @@ emailUnavailable: String = 'none';
   ngOnInit() {
   }
 
-  ngModelValidation () {
-    if (this.userName == '') {this.userName = null};
-    if (this.email == '') { this.email = null};
-    if (isNaN(this.age)) { this.age = null};
-    if (this.password == '') {this.password = null};
-    if (this.rePassword == '') {this.rePassword = null};
-    if(this.userName == null || this.email == null || this.password == null || this.age ==null || this.password == null || this.rePassword == null) {
-      this.missingParameters = '';}
+  ngModelValidation() {
+    if (this.userName === '') {this.userName = null; }
+    if (this.email === '') { this.email = null; }
+    if (isNaN(this.age)) { this.age = null; }
+    if (this.password === '') {this.password = null; }
+    if (this.rePassword === '') {this.rePassword = null; }
+    if (this.userName === null || this.email === null || this.password === null ||
+      this.age === null || this.password === null || this.rePassword === null) {
+      this.missingParameters = ''; }
     if (this.password.length < 6 || this.rePassword.length < 6) {
         this.password = null;
         this.rePassword = null;
         this. missingParametersPassword = '';
     }
-    if (this.password != this.rePassword) {
+    if (this.password !== this.rePassword) {
       this.password = null;
       this.rePassword = null;
       this.missingMatchParametersPassword = '';
@@ -52,46 +55,51 @@ emailUnavailable: String = 'none';
   }
 
   checkExistingUser() {
-    if (this.userName == '') {
+    if (this.userName === '') {
       this.userName = null;
       this.nameAvailable = this.nameUnavailable = 'none';
     }
-    if (this.userName != null) {
+    if (this.userName !== null) {
   this.userService.findByName(this.userName).subscribe(nameInput => {
     if (nameInput == null) {
       this.nameUnavailable = 'none';
       this.nameAvailable = '';
-    }
-    else {
+      this.booleanUsernameAvailable = false;
+    } else {
       this.nameAvailable = 'none';
       this.nameUnavailable = '';
+      this.booleanUsernameAvailable = true;
     }
-  }) 
+  });
 }
 }
 
 checkExistingEmail() {
-  if (this.email == '') {
+  if (this.email === '') {
     this.email = null;
   }
 this.userService.findByEmail(this.email).subscribe(emailInput => {
-  if (emailInput != null) {
+  if (emailInput !== null) {
     this.emailUnavailable = '';
-  }
-  else {
+    this.booleanEmailAvailable = false;
+  } else { 
     this.emailUnavailable = 'none';
+    this.booleanEmailAvailable = true;
   }
- }) 
+});
 }
 
   createNewUser() {
     this.ngModelValidation();
-    if (this.userName != null && this.email != null && this.age != null && this.password != null){
-      var user = new User (this.userName, this.password, this.email, this.age);
+    if (this.userName != null && this.email != null && this.age != null && this.password != null 
+      && this.booleanEmailAvailable === true && this.booleanUsernameAvailable === true){
+      const user = new User (this.userName, this.password, this.email, this.age);
       this.userService.newUser(user).subscribe(x => console.log(x));
       this.missingParameters = 'none';
       this.missingParametersPassword = 'none';
+      this.missingMatchParametersPassword = 'none';
       this.userCreatedSucess = '';
-    }  
+      window.scrollTo(0, 0);
+    }
 }
 }
